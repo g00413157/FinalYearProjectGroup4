eda613b4a788e1b06bdacec340e9330b41fcdcb7
g00415754 <dianakudrjavceva13@gmail.com>
2025-11-05 18:16:32 +0000

    Icons and animation animated for navbar

--- thryft-app/src/components/NavigationBar.jsx
+++ thryft-app/src/components/NavigationBar.jsx
@@ -1,35 +1,70 @@
-import React from 'react'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import React from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+import { GiShoppingBag, GiClothes } from 'react-icons/gi';
+import { BsHouseFill, BsPersonCircle } from 'react-icons/bs';
+import '../styles/NavigationBar.css';
 
 function NavigationBar() {
-    return (
-        <Navbar bg="dark" variant="dark" fixed="bottom">
-            <Container className="justify-content-around text-center">
-                <Nav className="w-100 justify-content-around">
-                    <Nav.Link href="/" className="d-flex flex-column align-items-center text-white">
-                        <i className="bi bi-house-door-fill fs-4"></i>
-                        <span className="small">Home</span>
-                    </Nav.Link>
+  const location = useLocation();
 
-                    <Nav.Link href="/shop" className="d-flex flex-column align-items-center text-white">
-                        <i className="bi bi-bag-heart-fill fs-4"></i>
-                        <span className="small">Shop</span>
-                    </Nav.Link>
+  const getActiveClass = (path) => {
+    return location.pathname === path ? 'active' : '';
+  };
 
-                    <Nav.Link href="/closet" className="d-flex flex-column align-items-center text-white">
-                        <i className="bi bi-person-standing-dress fs-4"></i>
-                        <span className="small">Closet</span>
-                    </Nav.Link>
-                    
-                    <Nav.Link href="/account" className="d-flex flex-column align-items-center text-white">
-                        <i className="bi bi-person-circle fs-4"></i>
-                        <span className="small">Account</span>
-                    </Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  const getIconSize = (path, defaultSize = 24) => {
+    return location.pathname === path ? defaultSize + 4 : defaultSize;
+  };
+
+  return (
+    <Navbar bg="dark" variant="dark" fixed="bottom">
+      <Container className="justify-content-around text-center">
+        <Nav className="w-100 justify-content-around">
+
+          <Nav.Link
+            as={NavLink}
+            to="/"
+            className={`d-flex flex-column align-items-center ${getActiveClass('/')}`}
+          >
+            <BsHouseFill size={getIconSize('/')} className="mb-1" />
+            <span className="small">Home</span>
+            <div className="nav-underline"></div>
+          </Nav.Link>
+
+          <Nav.Link
+            as={NavLink}
+            to="/shop"
+            className={`d-flex flex-column align-items-center ${getActiveClass('/shop')}`}
+          >
+            <GiShoppingBag size={getIconSize('/shop')} className="mb-1" />
+            <span className="small">Shop</span>
+            <div className="nav-underline"></div>
+          </Nav.Link>
+
+          <Nav.Link
+            as={NavLink}
+            to="/closet"
+            className={`d-flex flex-column align-items-center ${getActiveClass('/closet')}`}
+          >
+            <GiClothes size={getIconSize('/closet')} className="mb-1" />
+            <span className="small">Closet</span>
+            <div className="nav-underline"></div>
+          </Nav.Link>
+
+          <Nav.Link
+            as={NavLink}
+            to="/account"
+            className={`d-flex flex-column align-items-center ${getActiveClass('/account')}`}
+          >
+            <BsPersonCircle size={getIconSize('/account')} className="mb-1" />
+            <span className="small">Account</span>
+            <div className="nav-underline"></div>
+          </Nav.Link>
+
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
